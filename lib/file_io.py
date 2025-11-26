fd4dd3ae063869b11b66739629cf9d322ef2cb03
Rodney Swaji <rodney.swaji@student.moringaschool.com>
2025-11-26 15:01:03 +0300

    initial commit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name, content):
+    with open(f"{file_name}.txt", "w") as f:
+        f.write(content)
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name, content):
+    with open(f"{file_name}.txt", "a") as f:
+        f.write(content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", "r") as f:
+        return f.read()
